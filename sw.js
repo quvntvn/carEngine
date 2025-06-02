bd92662b564d7e278e5840c6f70b7cd344898698
quvntvn <hulkufe4@gmail.com>
2025-06-02 20:26:12 +0200

    cc

--- sw.js
+++ sw.js
@@ -1,14 +1,44 @@
 const CACHE_VERSION = "v1";
 const CACHE_NAME = `soundenginecar-${CACHE_VERSION}`;
 const ASSETS = [
-  "./soundenginecar/index.html", // Chemin corrigé
-  "./js/bundle.js",
-  "./js/sw-register.js",
-  "./manifest.webmanifest",
-  "./sounds/v8_loop.mp3",
-  "./sounds/f1_loop.mp3",
-  "./sounds/rally_loop.mp3",
-  "./sounds/ev_loop.mp3",
-  "./assets/icon-192.png",
-  "./assets/icon-512.png"
+  "/", // La racine du site (index.html)
+  "/index.html",
+  "/manifest.webmanifest",
+  "/sounds/v8_loop.mp3",
+  "/sounds/f1_loop.mp3",
+  "/sounds/rally_loop.mp3",
+  "/sounds/ev_loop.mp3",
+  "/assets/icon-192.png",
+  "/assets/icon-512.png"
 ];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
