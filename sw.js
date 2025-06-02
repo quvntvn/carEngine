bf5736730ea1473f4259b8e59191a408b0f8d608
quvntvn <hulkufe4@gmail.com>
2025-06-02 20:02:38 +0200

    Initial commit – PWA Sound Engine Car

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_VERSION = "v1";
+const CACHE_NAME = `soundenginecar-${CACHE_VERSION}`;
+const ASSETS = [
+  "./soundenginecar/index.html", // Chemin corrigé
+  "./js/bundle.js",
+  "./js/sw-register.js",
+  "./manifest.webmanifest",
+  "./sounds/v8_loop.mp3",
+  "./sounds/f1_loop.mp3",
+  "./sounds/rally_loop.mp3",
+  "./sounds/ev_loop.mp3",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png"
+];
